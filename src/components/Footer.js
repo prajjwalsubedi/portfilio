566197061d972b24fbe5f1507e24c5a2a6f4fb58
Prajjwal Subedi <66186489+prajjwalsubedi@users.noreply.github.com>
2026-08-07 08:27:22 -0400

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,38 @@
 function Footer() {
     return (
-      <div className="Footer">
-           
-            {/* <!-- Page Footer --> */}
-            <footer class="page-footer">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-sm-6">
-                            <p>Copyright
-                                <script>document.write(new Date().getFullYear())</script> &copy; <a
-                                    href="#" target="_blank">Prajjwal Subedi</a>
-                            </p>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="socials">
-                            <a class="social-item" href="https://www.linkedin.com/in/prajjwalsubedi"><i class="ti-linkedin"></i></a>
-                            <a class="social-item" href="https://twitter.com/prazwolsubedi"><i class="ti-twitter"></i></a>
-                            </div>
+        <footer className="page-footer">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-sm-6">
+                        <p>
+                            Copyright {new Date().getFullYear()} &copy;{" "}
+                            <a href="/">Prajjwal Subedi</a>
+                        </p>
+                    </div>
+
+                    <div className="col-sm-6">
+                        <div className="socials">
+                            <a
+                                className="social-item"
+                                href="https://www.linkedin.com/in/prajjwalsubedi"
+                                aria-label="LinkedIn"
+                            >
+                                <i className="ti-linkedin"></i>
+                            </a>
+
+                            <a
+                                className="social-item"
+                                href="https://twitter.com/prazwolsubedi"
+                                aria-label="Twitter"
+                            >
+                                <i className="ti-twitter"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
-            </footer>
-            {/* <!-- End of page footer --> */}
-      </div>
+            </div>
+        </footer>
     );
-  }
-  
-  export default Footer;
-  
+}
+
+export default Footer;
