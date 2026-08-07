af56eddc9e97adb64bcfec0ecbc273dbe483184b
Prajjwal Subedi <66186489+prajjwalsubedi@users.noreply.github.com>
2026-08-07 07:40:48 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -10,25 +10,40 @@ import Testmonial from './components/Testmonial';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import CGTDesk from './components/CGTDesk';
 
-
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
+/**
+ * Keep the existing portfolio as the home page, while serving CGT Desk from
+ * /cgt-desk without adding a routing dependency to this small React app.
+ */
+const isCGTDeskPage = window.location.pathname.replace(/\/+$/, '') === '/cgt-desk';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    <About />
-    <Service />
-    <Skills />
-    <Experience />
-    <Testmonial />
-    <Blog />
-    <Contact />
-    <Footer /> 
+    {isCGTDeskPage ? (
+      <>
+        <Header showHero={false} />
+        <CGTDesk />
+        <Footer />
+      </>
+    ) : (
+      <>
+        <Header />
+        <About />
+        <Service />
+        <Skills />
+        <Experience />
+        <Testmonial />
+        <Blog />
+        <Contact />
+        <Footer />
+      </>
+    )}
   </React.StrictMode>,
-
   document.getElementById('root')
 );
 
